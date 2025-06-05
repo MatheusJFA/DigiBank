d2b6d05543bfc95b37078b83a16e45d0e2f32aa2
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2025-06-05 09:13:16 -0300

    feat: Enable JPA auditing and web security in DigibankApplication

--- src/main/java/com/matheusjfa/Digibank/DigibankApplication.java
+++ src/main/java/com/matheusjfa/Digibank/DigibankApplication.java
@@ -2,12 +2,14 @@ package com.matheusjfa.Digibank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableWebSecurity
 public class DigibankApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DigibankApplication.class, args);
 	}
-
 }
