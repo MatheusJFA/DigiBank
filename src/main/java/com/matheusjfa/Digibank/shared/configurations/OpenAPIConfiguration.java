b56ac788fb2f855cee9ed12b263e02c16192d035
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2025-06-05 08:50:02 -0300

    feat: Add OpenAPIConfiguration

---
+++ src/main/java/com/matheusjfa/Digibank/shared/configurations/OpenAPIConfiguration.java
@@ -0,0 +1,31 @@
+package com.matheusjfa.Digibank.shared.configurations;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Digibank",
+                description = "Uma aplicação monolítica que simula um banco",
+                version = "0.0.1",
+                contact = @Contact(
+                        name = "Matheus José Ferreira de Aguiar",
+                        email = "matheusjfa@outlook.com"
+                )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenAPIConfiguration {
+}
