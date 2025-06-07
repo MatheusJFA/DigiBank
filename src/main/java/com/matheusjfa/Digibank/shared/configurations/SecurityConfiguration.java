93043bba5bea815eed88a9e396fe3a4a9e54fbf2
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2025-06-07 12:02:13 -0300

    feat: Implement JWT custom authentication filter and update security configuration

---
+++ src/main/java/com/matheusjfa/Digibank/shared/configurations/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.matheusjfa.Digibank.shared.configurations;
+
+import com.matheusjfa.Digibank.shared.security.JWTCustonAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JWTCustonAuthenticationFilter jwtCustonAuthenticationFilter
+    ) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+
+                            // Permite acesso a URLs públicas
+                            authorize.requestMatchers(
+                                    "/v2/api-docs/**",
+                                    "/v3/api-docs/**",
+                                    "/swagger-resources/**",
+                                    "/swagger-ui.html",
+                                    "/swagger-ui/**",
+                                    "/webjars/**",
+                                    "/actuator/**"
+                            ).permitAll();
+                            authorize.anyRequest().authenticated();
+                        }
+                )
+                .addFilter(jwtCustonAuthenticationFilter)
+                .build();
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        // Remove o prefixo "ROLE_" dos papéis
+        return new GrantedAuthorityDefaults("");
+    }
+}
---
+++ src/main/java/com/matheusjfa/Digibank/shared/security/JWTCustonAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package com.matheusjfa.Digibank.shared.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTCustonAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Aqui você pode implementar a lógica de autenticação JWT
+        // Por exemplo, verificar o token JWT no cabeçalho Authorization
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (isBearer(authorizationHeader)) {
+            String jwtToken = authorizationHeader.substring(7);
+            // Validar o token JWT e configurar a autenticação no contexto de segurança
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private static boolean isBearer(String authorizationHeader) {
+        return authorizationHeader != null && authorizationHeader.startsWith("Bearer ");
+    }
+}
