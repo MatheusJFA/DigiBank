46f9f53ccb6023b1ae3ed2658307d21f1a472e6f
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2025-06-08 14:29:46 -0300

    feat: Enhance JWT authentication filter to parse and validate tokens with roles

--- src/main/java/com/matheusjfa/Digibank/shared/security/JWTCustomAuthenticationFilter.java
+++ src/main/java/com/matheusjfa/Digibank/shared/security/JWTCustomAuthenticationFilter.java
@@ -1,35 +1,78 @@
 package com.matheusjfa.Digibank.shared.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Component
 public class JWTCustomAuthenticationFilter extends OncePerRequestFilter {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        // Aqui você pode implementar a lógica de autenticação JWT
-        // Por exemplo, verificar o token JWT no cabeçalho Authorization
-
         String authorizationHeader = request.getHeader("Authorization");
-        if (isBearer(authorizationHeader)) {
-            String jwtToken = authorizationHeader.substring(7);
-            // Validar o token JWT e configurar a autenticação no contexto de segurança
+        String token = getTokenFromHeader(authorizationHeader);
+
+        if(token != null && !token.isEmpty()) {
+            try {
+                Claims claims = parseToken(token);
+
+                String userID = claims.getSubject();
+                List<String> roles = claims.get("roles", List.class);
+
+                List<GrantedAuthority> authorities = roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userID, null, authorities);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (JwtException e) {
+               response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido ou expirado");
+            }
         }
 
         filterChain.doFilter(request, response);
     }
 
-    private static boolean isBearer(String authorizationHeader) {
-        return authorizationHeader != null && authorizationHeader.startsWith("Bearer ");
+    private Claims parseToken(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private String getTokenFromHeader(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
     }
 }
