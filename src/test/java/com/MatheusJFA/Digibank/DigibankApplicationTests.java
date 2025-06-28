eaa5cdf684e1f27faa5a72424367f51299c2b161
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2025-06-28 12:49:22 -0300

    Initial Commit

---
+++ src/test/java/com/MatheusJFA/Digibank/DigibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatheusJFA.Digibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
